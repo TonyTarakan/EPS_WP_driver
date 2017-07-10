1dca645d9fd02c527b11f3bc3ed5080917f752ea
vladimir <vgovseychuk@gmail.com>
2017-07-10 09:20:47 +0300

    The device in /dev/ appears after module init and disappears after module exit

--- esp8266_driver/esp8266_custom.c
+++ esp8266_driver/esp8266_custom.c
@@ -10,15 +10,37 @@
 #include <linux/slab.h>
 #include <linux/uaccess.h>
 
+static const struct file_operations esp_ctrl_fops = 
+{
+    .owner			= THIS_MODULE,
+    .llseek 		= no_llseek,
+};
+
+static struct miscdevice esp_ctrl = 
+{
+    .minor = MISC_DYNAMIC_MINOR,
+    .name = "esp8266_control",
+    .fops = &esp_ctrl_fops,
+};
+
 static int __init ktest_module_init( void )
 {
 	int res = 0;
+
+	esp_ctrl.minor = MISC_DYNAMIC_MINOR;
+    esp_ctrl.name = "esp8266_ctrl";
+    esp_ctrl.fops = &esp_ctrl_fops;
+
+    res = misc_register(&esp_ctrl);
+    if (res) 
+    	return res;
 
     printk( "ESP8266 under control\n" ); 
 	return res;
 } 
 static void __exit ktest_module_exit( void )
 {
+	misc_deregister( &esp_ctrl ); 
 	printk( "ESP8266 is free\n" ); 
 }
 
